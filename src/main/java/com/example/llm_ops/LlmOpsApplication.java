2f44915aa8dcb08b506a34f345eddbf9711e7c8d
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-07-14 15:53:31 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/example/llm_ops/LlmOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.llm_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmOpsApplication.class, args);
+	}
+
+}
