6d1b70600121ac5572c991c6ed47ddb516e975c1
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-07-15 12:21:30 +0900

    feat: Project 엔티티 구현

--- src/main/java/com/domain/Project.java
+++ src/main/java/com/domain/Project.java
@@ -5,21 +5,16 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Entity
-@Table(name = "log_tb")
+@Table(name = "project_tb")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Log extends TimeStamp{
+public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
-    private String type;
-
-    @Column
-    private String content;
+    private String name;
 }
--- src/main/java/com/service/LogReaderService.java
+++ src/main/java/com/service/LogReaderService.java
@@ -13,7 +13,7 @@ public class LogReaderService {
     private static final String LOG_FILE_PATH = "/path/to/buffer.b61d2f4a7a0373518cd314dbc2a03e543.log";
 
     @Scheduled(fixedRate = 60000) // 60초마다 실행
-    public void readLogFile() {
+    public void readLogFile(String type) {
         try {
             Path path = Paths.get(LOG_FILE_PATH);
             List<String> lines = Files.readAllLines(path);
